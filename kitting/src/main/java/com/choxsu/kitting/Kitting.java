2a998d65852027d0e38801ce3565eb9e23ba14c4
ChoxSu <2283546325@qq.com>
2018-01-17 18:05:01 +0800

    model

---
+++ kitting/src/main/java/com/choxsu/kitting/Kitting.java
@@ -0,0 +1,9 @@
+package com.choxsu.kitting;
+
+/**
+ * @author chox su
+ * @date 2018/01/17 17:59
+ */
+public class Kitting {
+
+}
---
+++ pay/src/main/java/com/choxsu/common/Start.java
@@ -0,0 +1,8 @@
+package com.choxsu.common;
+
+/**
+ * @author chox su
+ * @date 2018/01/17 18:04
+ */
+public class Start {
+}
