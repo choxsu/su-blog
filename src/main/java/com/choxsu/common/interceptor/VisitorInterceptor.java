2e87b5374d63ac2b562a98d2cca3e2642c6e39c9
ChoxSu <2283546325@qq.com>
2018-02-01 10:38:04 +0800

    請求 類修改

--- src/main/java/com/choxsu/common/interceptor/VisitorInterceptor.java
+++ src/main/java/com/choxsu/common/interceptor/VisitorInterceptor.java
@@ -1,5 +1,6 @@
 package com.choxsu.common.interceptor;
 
+import com.choxsu.common.entity.Visitor;
 import com.choxsu.common.kit.IpKit;
 import com.jfinal.aop.Interceptor;
 import com.jfinal.aop.Invocation;
@@ -28,14 +29,22 @@ public class VisitorInterceptor implements Interceptor {
         String ip = IpKit.getRealIp(request);
         String method = request.getMethod();
         String userAgent = request.getHeader("user-agent");
-        Record record = new Record();
-        record.set("ip", ip);
+//        Record record = new Record();
+//        record.set("ip", ip);
+//        record.set("url", url);
+//        record.set("method", method);
+//        record.set("client", userAgent);
+//        record.set("requestTime", new Date());
+//        boolean b = Db.save("visitor", record);
         LOGGER.info("请求ip:{}", ip);
-        record.set("url", url);
-        record.set("method", method);
-        record.set("client", userAgent);
-        record.set("requestTime", new Date());
-        boolean b = Db.save("visitor", record);
+
+        Visitor visitor = new Visitor();
+        visitor.setIp(ip);
+        visitor.setUrl(url);
+        visitor.setMethod(method);
+        visitor.setClient(userAgent);
+        visitor.setRequestTime(new Date());
+        boolean b = visitor.save();
         LOGGER.info("保存后返回的结果：{}", b);
         inv.invoke();
     }
