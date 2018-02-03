c1d9d315b70df612311fb1f9f0f279baac463c6e
ChoxSu <2283546325@qq.com>
2018-02-03 11:58:01 +0800

    init shiro

--- to-blog/src/main/java/com/choxsu/admin/controller/IndexAdminController.java
+++ to-blog/src/main/java/com/choxsu/admin/controller/IndexAdminController.java
@@ -1,6 +1,7 @@
 package com.choxsu.admin.controller;
 
 import com.choxsu.common.BaseController;
+import com.jfinal.kit.LogKit;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 
@@ -11,8 +12,8 @@ import org.apache.shiro.authz.annotation.RequiresPermissions;
 public class IndexAdminController extends BaseController {
 
     @RequiresPermissions("后台首页权限")
-    public void index(){
-
+    public void index() {
+        LogKit.info("后台首页权限");
         render("index.html");
     }
 }
