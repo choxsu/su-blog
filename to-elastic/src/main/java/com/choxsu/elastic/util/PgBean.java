1734581d76391274d6a128004f890d342167e4bc
ChoxSu <2283546325@qq.com>
2018-02-28 13:21:49 +0800

    pg bean

---
+++ to-elastic/src/main/java/com/choxsu/elastic/util/PgBean.java
@@ -0,0 +1,39 @@
+package com.choxsu.elastic.util;
+
+import com.jfinal.plugin.activerecord.Page;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author chox su
+ * @date 2018/02/28 9:29
+ */
+public class PgBean<T> {
+
+    /**
+     * 获取分页对象
+     *
+     * @param list  list集合对象
+     * @param page  当前页
+     * @param size  每页条数
+     * @param total 总条数
+     * @param <T>   泛型参数
+     * @return 分页对象
+     */
+    public static <T> Page<T> getPage(List<T> list, int page, int size, int total) {
+        return new Page<T>(list, page, size, ((total + size - 1) / size), total);
+    }
+
+    /**
+     * 获取分页对象
+     *
+     * @param page  当前页
+     * @param size  每页条数
+     * @param <T>   泛型参数
+     * @return 分页对象
+     */
+    public static <T> Page<T> getPage(int page, int size) {
+        return new Page<T>(new ArrayList<T>(), page, size, ((size - 1) / size), 0);
+    }
+}
