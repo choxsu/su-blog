8f77538d203a661d833f7568919616d83796c3fb
ChoxSu <2283546325@qq.com>
2018-03-05 19:33:30 +0800

    update

--- doc/sblog.sql
+++ doc/sblog.sql
@@ -143,4 +143,18 @@ CREATE TABLE `sensitive_words`  (
 -- ----------------------------
 INSERT INTO `sensitive_words` VALUES (1, '发票', 1, 'fapiao');
 
+-- ----------------------------
+-- Table structure for visitor
+-- ----------------------------
+DROP TABLE IF EXISTS `visitor`;
+CREATE TABLE `visitor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `ip` varchar(225) NOT NULL COMMENT '请求的IP地址',
+  `url` varchar(336) NOT NULL COMMENT '请求的页面路径',
+  `method` varchar(225) NOT NULL COMMENT '请求方法',
+  `client` varchar(225) DEFAULT NULL COMMENT '请求的客户端',
+  `requestTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '请求时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
 SET FOREIGN_KEY_CHECKS = 1;
