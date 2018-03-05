95a08d9bff97198babfe93f51f816e49d98a25dd
ChoxSu <2283546325@qq.com>
2018-03-05 12:54:12 +0800

    init model

--- to-blog/src/main/java/com/choxsu/api/blog/ApiBlogController.java
+++ to-blog/src/main/java/com/choxsu/api/blog/ApiBlogController.java
@@ -1,9 +1,12 @@
 package com.choxsu.api.blog;
 
+import com.choxsu.common.BaseController;
+
 /**
  * @author chox su
  * @date 2018/03/02 10:22
  */
-public class ApiBlogController {
+public class ApiBlogController extends BaseController{
+
 
 }
--- to-blog/src/main/java/com/choxsu/common/ApiRoutes.java
+++ to-blog/src/main/java/com/choxsu/common/ApiRoutes.java
@@ -1,9 +1,11 @@
 package com.choxsu.common;
 
-import com.choxsu.api.home.HomeController;
+import com.choxsu.api.blog.ApiBlogController;
+import com.choxsu.api.home.ApiHomeController;
 import com.jfinal.config.Routes;
 
 /**
+ * 接口路由
  * @author chox su
  * @date 2018/03/02 10:23
  */
@@ -12,7 +14,8 @@ public class ApiRoutes extends Routes {
     @Override
     public void config() {
 
-        add("/api/home", HomeController.class);
+        add("/api/v1/home", ApiHomeController.class);
+        add("/api/v1/blog", ApiBlogController.class);
 
 
     }
