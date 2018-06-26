2746628a85a19a521e18ee2f4fa1df3521e96b68
ChoxSu <2283546325@qq.com>
2018-06-26 09:23:44 +0800

    code update

---
+++ to-blog/src/main/java/com/choxsu/_admin/auth/AdminAuthKit.java
@@ -0,0 +1,58 @@
+package com.choxsu._admin.auth;
+
+
+import com.choxsu.common.entity.Account;
+
+/**
+ * 权限管理的 shared method 扩展
+ * <p>
+ * 作为 #role、#permission 指令的补充，支持 #else 块
+ * <p>
+ * <p>
+ * 使用示例：
+ * #if (hasRole("权限管理员", "CEO", "CTO"))
+ * ...
+ * #else
+ * ...
+ * #end
+ * <p>
+ * #if (hasPermission("/admin/project/edit"))
+ * ...
+ * #else
+ * ...
+ * #end
+ */
+public class AdminAuthKit {
+
+    /**
+     * 当前账号是否拥有某些角色
+     */
+    public boolean hasRole(String... roleNameArray) {
+        Account account = AdminAuthInterceptor.getThreadLocalAccount();
+        if (account != null && account.isStatusOk()) {
+            if (AdminAuthService.me.isSuperAdmin(account.getId()) ||
+                    AdminAuthService.me.hasRole(account.getId(), roleNameArray)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 是否拥有具体某个权限
+     */
+    public boolean hasPermission(String actionKey) {
+        Account account = AdminAuthInterceptor.getThreadLocalAccount();
+        if (account != null && account.isStatusOk()) {
+            if (AdminAuthService.me.isSuperAdmin(account.getId()) ||
+                    AdminAuthService.me.hasPermission(account.getId(), actionKey)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+
--- to-blog/src/main/java/com/choxsu/_admin/auth/AdminAuthService.java
+++ to-blog/src/main/java/com/choxsu/_admin/auth/AdminAuthService.java
@@ -1,5 +1,6 @@
 package com.choxsu._admin.auth;
 
+import com.jfinal.kit.Kv;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.SqlPara;
 
@@ -8,25 +9,39 @@ import com.jfinal.plugin.activerecord.SqlPara;
  */
 public class AdminAuthService {
 
-	public static final AdminAuthService me = new AdminAuthService();
+    public static final AdminAuthService me = new AdminAuthService();
 
-	/**
-	 * 是否为超级管理员，role.id 值为 1 的为超级管理员
-	 */
-	public boolean isSuperAdmin(int accountId) {
+    /**
+     * 是否为超级管理员，role.id 值为 1 的为超级管理员
+     */
+    public boolean isSuperAdmin(int accountId) {
 
-		SqlPara sp = Db.getSqlPara("admin.auth.isSuperAdmin", accountId);
-		Integer ret = Db.queryInt(sp.getSql(), sp.getPara());
-		return ret != null;
-	}
+        SqlPara sp = Db.getSqlPara("admin.auth.isSuperAdmin", accountId);
+        Integer ret = Db.queryInt(sp.getSql(), sp.getPara());
+        return ret != null;
+    }
 
-	/**
-	 * 是否拥有具体某个权限
-	 */
-	public boolean hasPermission(int accountId, String actionKey) {
-		SqlPara sp = Db.getSqlPara("admin.auth.hasPermission", actionKey, accountId);
-		Integer ret = Db.queryInt(sp.getSql(), sp.getPara());
-		return ret != null;
-	}
+    /**
+     * 是否拥有具体某个权限
+     */
+    public boolean hasPermission(int accountId, String actionKey) {
+        SqlPara sp = Db.getSqlPara("admin.auth.hasPermission", actionKey, accountId);
+        Integer ret = Db.queryInt(sp.getSql(), sp.getPara());
+        return ret != null;
+    }
+
+    /**
+     * 当前账号是否拥有某些角色
+     */
+    public boolean hasRole(int accountId, String[] roleNameArray) {
+        if (roleNameArray == null || roleNameArray.length == 0) {
+            return false;
+        }
+
+        Kv data = Kv.by("accountId", accountId).set("roleNameArray", roleNameArray);
+        SqlPara sp = Db.getSqlPara("admin.auth.hasRole", data);
+        Integer ret = Db.queryInt(sp.getSql(), sp.getPara());
+        return ret != null;
+    }
 }
 
