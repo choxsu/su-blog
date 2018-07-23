11cbb1b18fd276497f13d3fe5d02be7923f49856
ChoxSu <2283546325@qq.com>
2018-07-23 10:45:14 +0800

    code update

--- to-elastic/src/main/java/com/choxsu/Application.java
+++ to-elastic/src/main/java/com/choxsu/Application.java
@@ -1,12 +1,12 @@
-package com.choxsu.elastic;
+package com.choxsu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ElasticApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ElasticApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
---
+++ to-elastic/src/main/java/com/choxsu/mp/config/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.choxsu.mp.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author choxsu
+ */
+@Configuration
+@MapperScan("com.choxsu.*.mapper*")
+public class MybatisPlusConfig {
+
+    /*
+     * 分页插件，自动识别数据库类型
+     * 多租户，请参考官网【插件扩展】
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
