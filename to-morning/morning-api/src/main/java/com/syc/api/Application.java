eaf0a00f187fecd96908279eaffacfa9ca16ab60
choxsu <2283546325@qq.com>
2018-09-13 15:12:42 +0800

    update email config

---
+++ to-morning/morning-api/src/main/java/com/syc/api/Application.java
@@ -0,0 +1,15 @@
+package com.syc.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chox su
+ * @date 2018/01/17 18:04
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
