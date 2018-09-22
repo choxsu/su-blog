4d887629bc3de296bcf70d2d698685dc759e1adb
ChoxSu <2283546325@qq.com>
2018-09-22 21:33:55 +0800

    commit 首页controller

---
+++ to-morning/morning-api/src/main/java/com/syc/api/controller/blog/ApiIndexController.java
@@ -0,0 +1,34 @@
+package com.syc.api.controller.blog;
+
+import com.syc.api.controller.common.BaseApiController;
+import com.syc.model.result.Result;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/index/api")
+public class ApiIndexController extends BaseApiController {
+
+
+    /**
+     * @api {get} /index/api/v1/list #1、首页列表数据带分页
+     * @apiName 首页列表
+     * @apiGroup 首页相关接口
+     * @apiDescription 首页列表数据带分页,返回了所有的相关博客列表数据
+     *
+     * @apiParam {token} token 登录token信息
+     *
+     * @apiSuccess {int} code   提示代码 -1->权限不足 0->失败 1->成功
+     * @apiSuccess {String} msg 提示信息
+     * @apiSuccess {Object} data
+     *
+     */
+    @RequestMapping(value = "/v1/list", method = RequestMethod.GET)
+    public Result index(String token){
+
+        return Result.ok().setData(token);
+    }
+}
