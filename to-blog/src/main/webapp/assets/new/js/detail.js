71fe3d49e2805e8be11c2959decf25cc90cab82b
choxsu <2283546325@qq.com>
2018-12-18 15:20:43 +0800

    update ,

--- to-blog/src/main/webapp/assets/new/js/detail.js
+++ to-blog/src/main/webapp/assets/new/js/detail.js
@@ -1,8 +1,39 @@
 var Detail = {
     init: function () {
         this.zooming();
+        this.showTopImg();
+        this.returnTop();
     },
-    zooming: function () {//图片放大处理
+    showTopImg: function () {
+        window.onscroll = function () {
+            var t = document.documentElement.scrollTop || document.body.scrollTop;
+            var top = document.getElementsByClassName("return-top")[0];
+            if (t > 500) {
+                top.style.display = "block";
+            } else {
+                top.style.display = "none";
+            }
+        }
+    },
+    returnTop: function () {
+        var top = document.getElementsByClassName("return-top")[0];
+        top.onclick = function () {
+            var scrollTopTimer = setInterval(function () {
+                var top = document.body.scrollTop || document.documentElement.scrollTop;
+                var speed = top / 4;
+                if (document.body.scrollTop !== 0) {
+                    document.body.scrollTop -= speed;
+                } else {
+                    document.documentElement.scrollTop -= speed;
+                }
+                if (top === 0) {
+                    clearInterval(scrollTopTimer);
+                }
+            }, 30);
+        }
+    },
+    // 图片放大处理
+    zooming: function () {
         var img = document.getElementsByClassName("article-content")[0].getElementsByTagName("img");
         for (var i = 0; i < img.length; i++) {
             var imgElement = img[i];
