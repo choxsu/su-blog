a68630a024fff93d2aa07bd384ae031f5d6abf16
2283546325@qq.com <qiu1013>
2018-12-21 10:06:00 +0800

    update
    Signed-off-by: 2283546325@qq.com <qiu1013>

--- blog/src/main/webapp/assets/new/js/chosu.admin.js
+++ blog/src/main/webapp/assets/new/js/chosu.admin.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
     var pathname = location.pathname;
+    console.log(pathname)
+    console.log("================")
     var as = $(".nav-sidebar").find("li").find("a");
     for (var i = 0; i < as.length; i++) {
         var a = as[i];
-        if ($(a).attr("href") === pathname){
+        var href = $(a).attr("href");
+        /*console.log(href)
+        console.log(pathname.substring(0,href.length).indexOf(href));
+        console.log("--------------")*/
+        if (pathname === href && pathname.indexOf(href) !== -1){
             $(a).parent().addClass("active");
         }
     }
