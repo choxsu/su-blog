4c2866d52acdf743f930cf0748ee49114caa004d
choxsu <2283546325@qq.com>
2018-12-21 17:41:00 +0800

    登录后台代码优化，系统后台管理页面调整优化

--- blog/src/main/webapp/assets/new/js/chosu.admin.js
+++ blog/src/main/webapp/assets/new/js/chosu.admin.js
@@ -1,23 +1,197 @@
-$(document).ready(function () {
+/**
+ * 存放当前页面环境变量
+ */
+var env = {
+    ueditor: null,
+    deleteTarget: null
+};
 
-    var Admin = {
-        init: function () {
-            this.selectLeftTab();
-        },
-        selectLeftTab: function () {
-            var pathname = location.pathname;
-            var as = $(".nav-sidebar").find("li").find("a");
-            for (var i = 0; i < as.length; i++) {
-                var a = as[i];
-                var href = $(a).attr("href");
-                if (pathname === href && pathname.indexOf(href) !== -1){
-                    $(a).parent().addClass("active");
+var ShowUtil = {
+    showFailMsg: function (msg, callback) {
+        layer.msg(
+            msg
+            , {
+                shift: 6
+                , shade: 0.4
+                , time: 0
+                , offset: "140px"
+                , closeBtn: 1
+                , shadeClose: true
+                , maxWidth: "1000"
+            }
+            , callback
+        );
+    },
+    showAjaxActionMsg: function (shift, msg) {
+        layer.msg(msg, {
+                shift: shift
+                , shade: 0.4
+                , time: 2000
+                , offset: "140px"
+                , closeBtn: 1
+                , shadeClose: true
+                , maxWidth: "1000"
+            }, function () {
+                if (shift !== 6) {
+                    if (env.deleteTarget) {
+                        env.deleteTarget.remove();
+                        env.deleteTarget = null;
+                    } else {
+                        $.pjax.reload('#pjax-container', {});
+                    }
                 }
             }
-        }
-    };
+        );
+    }
+
+};
+
+var Admin = {
+    nprogressStart: function () {
+        NProgress.configure({showSpinner: false}).start();
+    },
+    nprogressDone: function () {
+        NProgress.done();
+    },
+    selectLeftTab: function () {
+        var pathname = location.pathname;
+        var ps = pathname.split("/");
+        $(".nav-sidebar li a[href]").each(function (index, element) {
+            var href = $(element).attr("href");
+
+            if (pathname === href) {
+                var currentMenu = $(".nav-sidebar li a[href='" + href + "']");
+                currentMenu.parent().addClass("active");
+            }
+        })
+        /*var as = $(".nav-sidebar li a[href]");
+        for (var i = 0; i < as.length; i++) {
+            var a = as[i];
+            var href = $(a).attr("href");
+            if (pathname === href && pathname.indexOf(href) !== -1) {
+                $(a).parent().addClass("active");
+            } else {
+                $(a).parent().removeClass("active");
+            }
+        }*/
+    },
+    sendPjax: function (url, container, extData) {
+        $.pjax({
+            url: url,
+            container: container,
+            extData: extData
+        });
+    },
+    deleteArticle: function (event) {
+        event.preventDefault();
+        var $this = $(this);
+        env.deleteTarget = $this.closest("tr");	// 传递给 showAjaxActionMsg() 删除一行数据
+        var action = $this.attr("data-action");
+        var title = $this.attr("data-title");
+        Admin.confirmAjaxAction("确定删除【" + title + "】?删除后无法恢复！", action);
+    },
+    confirmAjaxAction: function (msg, operationUrl) {
+        layer.confirm(msg, {
+            icon: 0
+            , title: ''                                    // 设置为空串时，title消失，并自动切换关闭按钮样式，比较好的体验
+            , shade: 0.4
+            , offset: "139px"
+        }, function (index) {                            // 只有点确定后才会回调该方法
+            // location.href = operationUrl;     // 操作是一个 GET 链接请求，并非 ajax
+            Admin.ajaxAction(operationUrl);
+            layer.close(index);                           // 需要调用 layer.close(index) 才能关闭对话框
+        });
+    },
+    ajaxAction: function (url) {
+        $.ajax(url, {
+            type: "GET"
+            , cache: false
+            , dataType: "json"
+            // , data: {	}
+            , beforeSend: function () {
+            }
+            , error: function (ret) {
+                alert(ret.statusText);
+            }
+            , success: function (ret) {
+                if (ret.state === "ok") {
+                    ShowUtil.showAjaxActionMsg(0, ret.msg);
+                } else {
+                    ShowUtil.showAjaxActionMsg(6, ret.msg);
+                }
+            }
+        });
+    },
+};
+
+var Menu = {
+    clickMenu: function (event) {
+        event.preventDefault();	// 取代 return false 防止页面跳转
+        var $this = $(this);
+        var url = $this.attr("href");
+        Admin.sendPjax(url, "#pjax-container");
+        // 设置当前选中菜单样式
+        $(".nav-sidebar li").removeClass("active");
+        $this.parent().addClass("active");
+    }
+}
 
 
-    Admin.init();
+$(document).ready(function () {
+    Admin.nprogressStart();
+    // 绑定首页菜单事件
+    $(".nav-sidebar li a").bind("click", Menu.clickMenu);
+    // table 操作栏按钮绑定 click 事件，动态添加的元素必须使用 $(...).on(...) 才能绑定
+    $('#pjax-container').on("click", "a[data-delete]", Admin.deleteArticle);
+    // 进度条效果
+    $(document).on('pjax:start', function () {
+        Admin.nprogressStart()
+    });
+    $(document).on('pjax:end', function () {
+        Admin.nprogressDone()
+    });
+    //页面加载完成关闭进度条
+    $(window).load(function () {
+        Admin.nprogressDone();
+    });
 
+    //选中当前菜单
+    Admin.selectLeftTab();
 });
+
+
+var Magic = {
+
+    initMagicInput: function (prepareAction) {
+        var magicInput = $("input.mgc-switch,input.mgc");
+
+        // 锁定开关绑定事件
+        magicInput.on("click", function (event) {
+            var $this = $(event.target);	// 或者 $(this)
+            var state = $this.get(0).checked ? true : false;
+            var action = prepareAction($this, state);
+
+            $.ajax(action.url, {
+                type: "POST"
+                , cache: false
+                , dataType: "json"
+                , data: action.data
+                , error: function (ret) {
+                    alert(ret.statusText);
+                    Magic.resetMagicInput($this);
+                }
+                , success: function (ret) {
+                    if (ret.state === "fail") {
+                        ShowUtil.showFailMsg(ret.msg);
+                        Magic.resetMagicInput($this);
+                    }
+                }
+            });
+        });
+    },
+    resetMagicInput: function ($checkbox) {
+        var checkbox = $checkbox.get(0);
+        checkbox.checked = !checkbox.checked;
+    }
+};
+
