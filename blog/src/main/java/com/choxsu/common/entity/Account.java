1a084249cdf9c4491382fd55ffaafdc7749d2c7c
choxsu <2283546325@qq.com>
2018-12-24 11:49:25 +0800

    登录后台代码优化，系统后台管理页面调整优化

--- blog/src/main/java/com/choxsu/common/entity/Account.java
+++ blog/src/main/java/com/choxsu/common/entity/Account.java
@@ -9,7 +9,7 @@ import com.choxsu.common.safe.JsoupFilter;
 @SuppressWarnings("serial")
 public class Account extends BaseAccount<Account> {
 
-    public static final String AVATAR_NO_AVATAR = "x.jpg";    // 刚注册时使用默认头像
+    public static final String AVATAR_NO_AVATAR = "default.png";    // 刚注册时使用默认头像
 
     public static final int STATUS_LOCK_ID = -1;    // 锁定账号，无法做任何事情
     public static final int STATUS_REG = 0;            // 注册、未激活
