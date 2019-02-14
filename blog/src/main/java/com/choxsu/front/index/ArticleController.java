3f3d89e9a687f1c1840ce1aa48ec958239c8fece
choxsu <2283546325@qq.com>
2019-02-14 14:25:40 +0800

    路由规则修改

--- blog/src/main/java/com/choxsu/front/index/ArticleController.java
+++ blog/src/main/java/com/choxsu/front/index/ArticleController.java
@@ -17,11 +17,6 @@ public class ArticleController extends BaseController {
     @Inject
     ArticleService articleService;
 
-    @ActionKey("/")
-    public void redirectToArticle() {
-        redirect("/article");
-    }
-
     public void index() {
         int page = getParaToInt("p", 1);
         Page<Record> blogPage = articleService.findArticles(page, 10, null);
