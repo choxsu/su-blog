22db5424adbc20aea996601af21b9f535e1bdb08
choxsu <choxsu@163.com>
2019-05-09 22:13:27 +0800

    引入 ant-design-vue

--- front/src/main.js
+++ front/src/main.js
@@ -1,16 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Button from "ant-design-vue/lib/button"
-import "ant-design-vue/dist/antd.css"
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 
-
-
-Vue.component(Button.name, Button);
+Vue.use(Antd)
 
 Vue.config.productionTip = false
 
 
-
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
