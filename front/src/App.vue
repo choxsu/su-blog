a8310ad8e63f4b4a646048898b639a6dc01350d0
choxsu <choxsu@163.com>
2019-05-09 21:54:41 +0800

    引入 ant-design-vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <a-button type="primary">按钮</a-button>
+    </div>
   </div>
 </template>
 
