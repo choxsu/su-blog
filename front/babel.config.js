2b07afac8a69344a52c1f94d7df56bd7802b7864
choxsu <choxsu@163.com>
2019-05-10 15:15:15 +0800

    干掉 bootstrap

--- front/babel.config.js
+++ front/babel.config.js
@@ -1,17 +1,5 @@
-const webpack = require("webpack");
-
 module.exports = {
     presets: [
         '@vue/app'
-    ],
-    configureWebpack: {
-        plugins: [
-            new webpack.ProvidePlugin({
-                $: 'jquery',
-                jQuery: 'jquery',
-                'window.jQuery': 'jquery',
-                Popper: ['popper.js', 'default']
-            })
-        ]
-    }
+    ]
 }
