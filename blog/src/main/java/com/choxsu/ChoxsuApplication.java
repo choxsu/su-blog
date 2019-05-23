37b066428936d0828d967d1702679a2789cf20f0
choxsu <choxsu@163.com>
2019-05-23 17:39:10 +0800

    包结构优化

--- blog/src/main/java/com/choxsu/ChoxsuApplication.java
+++ blog/src/main/java/com/choxsu/ChoxsuApplication.java
@@ -7,11 +7,7 @@ import com.jfinal.server.undertow.UndertowServer;
  * @author choxsu
  */
 public class ChoxsuApplication {
-
-
     public static void main(String[] args) {
         UndertowServer.create(ChoxsuConfig.class).start();
     }
-
-
 }
