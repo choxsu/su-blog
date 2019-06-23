da58b6d4610df28a6ba2f3d9b15632c5465aea0c
ChoxSu <2283546325@qq.com>
2019-06-23 12:23:04 +0800

    code gen

--- blog/src/main/java/com/choxsu/_admin/code/CodeConfigController.java
+++ blog/src/main/java/com/choxsu/_admin/code/CodeConfigController.java
@@ -1,11 +1,25 @@
 package com.choxsu._admin.code;
 
 import com.choxsu.common.base.BaseController;
+import com.choxsu.common.entity.CodeConfig;
+import com.jfinal.aop.Inject;
+import com.jfinal.core.paragetter.Para;
+import com.jfinal.kit.Ret;
 
 public class CodeConfigController extends BaseController {
 
+    @Inject
+    CodeConfigService ccs;
+
     public void index(){
+        CodeConfig codeConfig = ccs.findById(1L);
+        set("codeConfig", codeConfig);
         render("index_config.html");
+    }
+
+    public void update(@Para("") CodeConfig codeConfig){
+        Ret ret = ccs.updateData(codeConfig);
+        renderJson(ret);
     }
 
 }
